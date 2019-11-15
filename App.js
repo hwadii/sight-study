7c69e1ca53d9ff990656e76e33029cc068ec2055
adamcolas <colas.adam@gmail.com>
2019-11-15 16:11:42 +0100

    upadte db

--- App.js
+++ App.js
@@ -4,11 +4,8 @@ import SignIn from "./src/SignIn";
 import SignUp from "./src/SignUp";
 import Score from "./src/Score";
 import Test from "./src/Test";
-<<<<<<< HEAD
 import TestScreen from "./src/TestScreen";
-=======
 import Menu from "./src/Menu";
->>>>>>> 4c5e3414d4908cb6ffab544dbeacc39c3c3b5ec7
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
@@ -16,12 +13,9 @@ const Routes = {
   SignIn,
   SignUp,
   Score,
-<<<<<<< HEAD
   Test,
-  TestScreen
-=======
+  TestScreen,
   Menu
->>>>>>> 4c5e3414d4908cb6ffab544dbeacc39c3c3b5ec7
 };
 
 const MainNavigator = createStackNavigator(
