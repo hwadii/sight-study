90b47b6843cefcc2198150c3531bd20e22178cc4
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-11-17 00:24:27 +0100

    Add image assets

--- src/Card.js
+++ src/Card.js
@@ -2,54 +2,60 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { TouchableHighlight } from "react-native-gesture-handler";
 
+// TODO: Add SVG Icons for cards.
+
 export default class Card extends React.Component {
-  render() {
-    const { title, description, route, navigate } = this.props;
-    // console.log(navigate(route));
-    console.log(this.props);
-    return (
-      // <View style={styles.card}>
-      <TouchableHighlight
-        onPress={this.handlePress}
-        style={styles.button}
-      >
-        <View>
-          <Text style={styles.header}>{title}</Text>
-          <Text style={styles.description}>{description}</Text>
-        </View>
-      </TouchableHighlight>
-      // </View>
-    );
+  constructor(props) {
+    super(props);
   }
 
-  handlePress() {
-    console.log(this.props.route)
+  render() {
+    const { title, description, route, navigate } = this.props;
+    return (
+      <View style={styles.card}>
+        <TouchableHighlight
+          underlayColor="#fff"
+          onPress={() => navigate(route)}
+        >
+          <Content title={title} description={description} />
+        </TouchableHighlight>
+      </View>
+    );
   }
+}
+
+function Content({ title, description }) {
+  return (
+    <View style={styles.content}>
+      <Text style={styles.header}>{title}</Text>
+      <Text style={styles.description}>{description}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   card: {
-    // flex: 1,
-    marginVertical: 5,
-    width: "100%",
-    shadowColor: "#979797",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 10
+    flex: 1,
+    justifyContent: "center",
+    marginVertical: 10,
+    height: 200,
+    width: "80%"
   },
   header: {
     fontSize: 22,
-    // paddingVertical: "1rem",
+    paddingVertical: 5,
     fontWeight: "bold",
     textAlign: "center"
   },
-  button: {
-    paddingVertical: ".5rem",
-    paddingHorizontal: ".5rem"
+  content: {
+    height: "100%",
+    justifyContent: "center",
+    backgroundColor: "#f5f5f5",
+    borderRadius: 5
   },
   description: {
     fontSize: 16,
-    color: "#979797"
+    textAlign: "center",
+    color: "#979797",
   }
 });
