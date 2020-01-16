9e3a835f232eda3e7d66b9c388c627caca148f7f
Sylvain Huss <sylvain.huss@laposte.net>
2020-01-16 09:49:03 +0100

    volume set to max at begining

--- android/app/src/main/java/host/exp/exponent/MainApplication.java
+++ android/app/src/main/java/host/exp/exponent/MainApplication.java
@@ -15,6 +15,7 @@ import com.rnfs.RNFSPackage;
 
 // Needed for `react-native link`
 // import com.facebook.react.ReactApplication;
+import com.ninty.system.setting.SystemSettingPackage;
 import com.robinpowered.react.ScreenBrightness.ScreenBrightnessPackage;
 import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends ExpoApplication implements AppLoaderPackage
 
         // Needed for `react-native link`
         // new MainReactPackage(),
+            new SystemSettingPackage(),
             new ScreenBrightnessPackage(),
             new RNPermissionsPackage(),
             // new SvgPackage(),
