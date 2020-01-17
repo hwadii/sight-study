505602f2738b2b20d90656ea331254b1b247499c
Sylvain Huss <sylvain.huss@laposte.net>
2020-01-17 19:41:23 +0100

    brightness and portrait lock

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "expbbdaf6d098bb4baeb9a6eaa247b0ce83://10.120.130.247:19006";
+  public static final String DEVELOPMENT_URL = "expbbdaf6d098bb4baeb9a6eaa247b0ce83://192.168.43.195:19000";
 
 }
