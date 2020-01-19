b109d32dd8266086b74e658153d6b9a99b290de2
adamcolas <colas.adam@gmail.com>
2020-01-19 15:38:48 +0100

    c'est moche

--- src/Settings.js
+++ src/Settings.js
@@ -15,36 +15,36 @@ export default class Settings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      1 :"",
-      2 :"",
-      3 :"",
-      4 :"",
-      tableau :[]
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      tableau: []
     };
     this.handleChangeField = this.handleChangeField.bind(this);
     this.handleModifDistance = this.handleModifDistance.bind(this);
     this.handleChangeAcuite = this.handleChangeAcuite.bind(this)
     this.HandleSetAcuites = this.HandleSetAcuites.bind(this)
     this.props.navigation.navigate = this.props.navigation.navigate.bind(this);
-    
+
   }
 
   handleChangeField(e, field) {
     this.setState({ [field]: e.nativeEvent.text });
   }
 
-  async HandleSetAcuites(){
-    const {tableau} = this.state
+  async HandleSetAcuites() {
+    const { tableau } = this.state
     console.log(tableau)
     await setAcuites(tableau);
     const { navigate } = this.props.navigation;
     navigate("SetUser");
   }
 
-  handleChangeAcuite(e, i){
-    let {tableau} = this.state
+  handleChangeAcuite(e, i) {
+    let { tableau } = this.state
     tableau[i] = e.nativeEvent.text
-    this.setState({tableau: tableau})
+    this.setState({ tableau: tableau })
   }
 
   async handleModifDistance() {
@@ -54,18 +54,18 @@ export default class Settings extends React.Component {
 
   async componentDidMount() {
 
-      this.setState({
-        tableau: await getAcuites(),
-      });
+    this.setState({
+      tableau: await getAcuites(),
+    });
   }
 
   render() {
-    const {tableau} = this.state
+    const { tableau } = this.state
 
     return (
       <View style={styles.container}>
         <Form
-          tableau= {tableau}
+          tableau={tableau}
           navigate={this.props.navigation.navigate}
           HandleSetAcuites={this.HandleSetAcuites}
           handleChangeAcuite={this.handleChangeAcuite}
@@ -75,7 +75,7 @@ export default class Settings extends React.Component {
   }
 }
 
-function Form({tableau, HandleSetAcuites, handleChangeAcuite }) {
+function Form({ tableau, HandleSetAcuites, handleChangeAcuite }) {
   return (
     <View style={styles.form}>
       <Board tableau={tableau} handleChangeAcuite={handleChangeAcuite}></Board>
@@ -92,35 +92,40 @@ function Form({tableau, HandleSetAcuites, handleChangeAcuite }) {
 function Field({ value, label, handler }) {
   return (
     <View style={styles.noAccount}>
-      <Text style={common.inputsLabels}>{label}</Text>
-      <TextInput
-        defaultValue={value}
-        style={common.inputs}
-        autoCorrect={false}
-        defaultValue={value}
-        onChange={handler}
-      />
+      <View style={{ paddingTop: 10, paddingVertical: 10 }}>
+        <Text style={styles.label}>{label}</Text>
+      </View>
+      <View style={{ textAlignVertical: 'center',  paddingTop: 10, paddingLeft: 10 }}>
+        <TextInput
+          defaultValue={value}
+          //style={common.inputs}
+          style={styles.input}
+          autoCorrect={false}
+          defaultValue={value}
+          onChange={handler}
+        />
+      </View>
     </View>
   );
 }
 
 
-function Board ({tableau,handleChangeAcuite}){
-  return(
+function Board({ tableau, handleChangeAcuite }) {
+  return (
     <>
-      <Text>valeurs des acuités visuels a tester</Text>
-      <Field value={tableau[0]} label=" 1 acuité" handler={(e)=>handleChangeAcuite(e,0)}></Field>
-      <Field value={tableau[1]} label=" 2 acuité" handler={(e)=>handleChangeAcuite(e,1)}></Field>
-      <Field value={tableau[2]} label=" 3 acuité" handler={(e)=>handleChangeAcuite(e,2)}></Field>
-      <Field value={tableau[3]} label=" 4 acuité" handler={(e)=>handleChangeAcuite(e,3)}></Field>
-      <Field value={tableau[4]} label=" 5 acuité" handler={(e)=>handleChangeAcuite(e,4)}></Field>
-      <Field value={tableau[5]} label=" 6 acuité" handler={(e)=>handleChangeAcuite(e,5)}></Field>
-      <Field value={tableau[6]} label=" 7 acuité" handler={(e)=>handleChangeAcuite(e,6)}></Field>
-      <Field value={tableau[7]} label=" 8 acuité" handler={(e)=>handleChangeAcuite(e,7)}></Field>
-      <Field value={tableau[8]} label=" 9 acuité" handler={(e)=>handleChangeAcuite(e,8)}></Field>
-      <Field value={tableau[9]} label=" 10 acuité" handler={(e)=>handleChangeAcuite(e,9)}></Field>
-      <Field value={tableau[10]} label=" 11 acuité" handler={(e)=>handleChangeAcuite(e,10)}></Field>
-      <Field value={tableau[11]} label=" 12 acuité" handler={(e)=>handleChangeAcuite(e,11)}></Field>
+      <Text style={styles.header}>valeurs des acuités visuels a tester</Text>
+      <Field value={tableau[0]} label=" 1 acuité" handler={(e) => handleChangeAcuite(e, 0)}></Field>
+      <Field value={tableau[1]} label=" 2 acuité" handler={(e) => handleChangeAcuite(e, 1)}></Field>
+      <Field value={tableau[2]} label=" 3 acuité" handler={(e) => handleChangeAcuite(e, 2)}></Field>
+      <Field value={tableau[3]} label=" 4 acuité" handler={(e) => handleChangeAcuite(e, 3)}></Field>
+      <Field value={tableau[4]} label=" 5 acuité" handler={(e) => handleChangeAcuite(e, 4)}></Field>
+      <Field value={tableau[5]} label=" 6 acuité" handler={(e) => handleChangeAcuite(e, 5)}></Field>
+      <Field value={tableau[6]} label=" 7 acuité" handler={(e) => handleChangeAcuite(e, 6)}></Field>
+      <Field value={tableau[7]} label=" 8 acuité" handler={(e) => handleChangeAcuite(e, 7)}></Field>
+      <Field value={tableau[8]} label=" 9 acuité" handler={(e) => handleChangeAcuite(e, 8)}></Field>
+      <Field value={tableau[9]} label=" 10 acuité" handler={(e) => handleChangeAcuite(e, 9)}></Field>
+      <Field value={tableau[10]} label=" 11 acuité" handler={(e) => handleChangeAcuite(e, 10)}></Field>
+      <Field value={tableau[11]} label=" 12 acuité" handler={(e) => handleChangeAcuite(e, 11)}></Field>
     </>
   )
 }
@@ -137,7 +142,7 @@ const styles = StyleSheet.create({
     width: scale(320),
     maxWidth: Dimensions.get("window").width
   },
-  board:{
+  board: {
     flexDirection: "row",
     justifyContent: "center",
     marginVertical: 10
@@ -162,6 +167,24 @@ const styles = StyleSheet.create({
   noAccount: {
     flexDirection: "row",
     justifyContent: "center",
-    marginVertical: 10
+    marginVertical: 10,
+    textAlignVertical: 'center'
+  },
+  input:{
+    borderColor: "#CCCCCC",
+    borderWidth: 1,
+    borderRadius: 3,
+    fontSize: 25,
+    height: 40,
+    paddingLeft: 5,
+    paddingRight: 5,
+    marginBottom: 6,
+    width: 50
+  },
+  label:{
+    fontSize: 25,
+    marginTop: 7,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
