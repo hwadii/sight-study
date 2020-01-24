787315d6aa2bb1e4c642a34e1ef2505ede09b5f5
hwadii <wadii.hajji@etu.univ-amu.fr>
2020-01-24 08:50:00 +0100

    Lasto build 1.0

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "expbbdaf6d098bb4baeb9a6eaa247b0ce83://192.168.43.195:19006";
+  public static final String DEVELOPMENT_URL = "expbbdaf6d098bb4baeb9a6eaa247b0ce83://192.168.1.7:80";
 
 }
